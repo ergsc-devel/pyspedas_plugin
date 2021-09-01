0fbf2b6e18719ad3c33db7195eaf5009e48c19d8
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-01 15:28:01 +0900

    Create pwe_ofa.py
    Split pwe_ofa() in pwe.py to pwe_ofa.py for readability
    Added "from load import load" for made load.py
    Added "import cdflib" to show ror
    Added "vernames" argument for load function
    Added processing to show ror

---
+++ pwe/pwe_ofa.py
@@ -0,0 +1,122 @@
+
+#from pyspedas.erg.load import load
+from load import load
+from pytplot import options, clip
+import cdflib
+
+def pwe_ofa(trange=['2017-04-01', '2017-04-02'],
+        datatype='spec', 
+        level='l2', 
+        suffix='',  
+        get_support_data=False, 
+        varformat=None,
+        varnames=[],
+        downloadonly=False,
+        notplot=False,
+        no_update=False,
+        uname=None,
+        passwd=None,
+        time_clip=False):
+    """
+    This function loads data from the PWE experiment from the Arase mission
+    
+    Parameters:
+        trange : list of str
+            time range of interest [starttime, endtime] with the format 
+            'YYYY-MM-DD','YYYY-MM-DD'] or to specify more or less than a day 
+            ['YYYY-MM-DD/hh:mm:ss','YYYY-MM-DD/hh:mm:ss']
+
+        datatype: str
+            Data type; Valid options:
+
+        level: str
+            Data level; Valid options:
+
+        suffix: str
+            The tplot variable names will be given this suffix.  By default, 
+            no suffix is added.
+
+        get_support_data: bool
+            Data with an attribute "VAR_TYPE" with a value of "support_data"
+            will be loaded into tplot.  By default, only loads in data with a 
+            "VAR_TYPE" attribute of "data".
+
+        varformat: str
+            The file variable formats to load into tplot.  Wildcard character
+            "*" is accepted.  By default, all variables are loaded in.
+
+        varnames: list of str
+            List of variable names to load (if not specified,
+            all data variables are loaded)
+
+        downloadonly: bool
+            Set this flag to download the CDF files, but not load them into 
+            tplot variables
+
+        notplot: bool
+            Return the data in hash tables instead of creating tplot variables
+
+        no_update: bool
+            If set, only load data from your local cache
+
+        time_clip: bool
+            Time clip the variables to exactly the range specified in the trange keyword
+
+    Returns:
+        List of tplot variables created.
+
+    """
+    loaded_data = load(instrument='pwe_ofa', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
+    
+    if len(loaded_data) > 0:
+
+    
+        out_files = load(instrument='pwe_ofa', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
+        cdf_file = cdflib.CDF(out_files[0])
+        gatt = cdf_file.globalattsget()
+
+        # --- print PI info and rules of the road
+
+        print(' ')
+        print(' ')
+        print('**************************************************************************')
+        print(gatt["LOGICAL_SOURCE_DESCRIPTION"])
+        print('')
+        print('Information about ERG PWE OFA')
+        print('')        
+        print('PI: ', gatt['PI_NAME'])
+        print("Affiliation: "+gatt["PI_AFFILIATION"])
+        print('')
+        print('RoR of ERG project common: https://ergsc.isee.nagoya-u.ac.jp/data_info/rules_of_the_road.shtml.en')
+        print('RoR of PWE/OFA: https://ergsc.isee.nagoya-u.ac.jp/mw/index.php/ErgSat/Pwe/Ofa')
+        print('')
+        print('Contact: erg_pwe_info at isee.nagoya-u.ac.jp')
+        print('**************************************************************************')
+
+
+    # set spectrogram plot option
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'Spec', 1)
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'Spec', 1)
+
+    # set y axis to logscale
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'ylog', 1)
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'ylog', 1)
+    
+    # set yrange
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'yrange', [0., 10.])
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'yrange', [0., 11.])
+    
+    # set z axis to logscale
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'zlog', 1)
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'zlog', 1)
+    
+    # set zrange
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'zrange', [1.0e-08, 1.0e-02])
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'zrange', [1.0e-04, 1.0e+02])
+    
+    # change colormap option
+    options('erg_pwe_ofa_'+level+'_E_spectra_132'+suffix,  'Colormap', 'jet')
+    options('erg_pwe_ofa_'+level+'_B_spectra_132'+suffix,  'Colormap', 'jet')
+    
+    
+    return loaded_data
