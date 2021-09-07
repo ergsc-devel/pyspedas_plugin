f231bd373e86a8acd54a8980a9a17831939b3b32
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-07 16:25:57 +0900

    Add ror argument, its processing, and its documentation in lepi.py

--- lepi/lepi.py
+++ lepi/lepi.py
@@ -17,7 +17,8 @@ def lepi(trange=['2017-07-01', '2017-07-02'],
         no_update=False,
         uname=None,
         passwd=None,
-        time_clip=False):
+        time_clip=False,
+        ror=True):
     """
     This function loads data from the LEP-i experiment from the Arase mission
     
@@ -63,6 +64,9 @@ def lepi(trange=['2017-07-01', '2017-07-02'],
         time_clip: bool
             Time clip the variables to exactly the range specified in the trange keyword
 
+        ror: bool
+            If set, print PI info and rules of the road
+
     Returns:
         List of tplot variables created.
 
@@ -71,7 +75,7 @@ def lepi(trange=['2017-07-01', '2017-07-02'],
     loaded_data = load(instrument='lepi', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
 
     
-    if len(loaded_data) > 0:
+    if len(loaded_data) > 0 and ror:
 
     
         out_files = load(instrument='lepi', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
