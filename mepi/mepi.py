93d87d3325766191a96ffce58bbbc94be7c3cc92
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-07 15:27:20 +0900

    Add ror argument, its processing, and its documentation in mepi.py

--- mepi/mepi.py
+++ mepi/mepi.py
@@ -16,7 +16,8 @@ def mepi(trange=['2017-03-27', '2017-03-28'],
         no_update=False,
         uname=None,
         passwd=None,
-        time_clip=False):
+        time_clip=False,
+        ror=True):
     """
     This function loads data from the MEP-i experiment from the Arase mission
     
@@ -62,6 +63,9 @@ def mepi(trange=['2017-03-27', '2017-03-28'],
         time_clip: bool
             Time clip the variables to exactly the range specified in the trange keyword
 
+        ror: bool
+            If set, print PI info and rules of the road
+
     Returns:
         List of tplot variables created.
 
@@ -69,7 +73,7 @@ def mepi(trange=['2017-03-27', '2017-03-28'],
 
     loaded_data = load(instrument='mepi', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
 
-    if len(loaded_data) > 0:
+    if len(loaded_data) > 0 and ror:
     
         out_files = load(instrument='mepi', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
         cdf_file = cdflib.CDF(out_files[0])
