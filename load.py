0a024cb22e8bab919bdec1a6f17d4071afee3c60
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-09 18:33:00 +0900

    Delete pathformat processing for hep, in load.py

--- load.py
+++ load.py
@@ -59,20 +59,6 @@ def load(trange=['2017-03-27', '2017-03-28'],
                 file_res=3600.
                 pathformat = 'satellite/erg/'+instrument+'/'+level+'/'+datatype+'/%Y/%m/erg_'+instrument+'_'+level+'_'+datatype+'_dsi_%Y%m%d%H_v??.??.cdf'
 
-        elif instrument == 'hep':
-            if level == 'l2':
-                pathformat = 'satellite/erg/'+instrument+'/'+level+'/'+datatype+'/%Y/%m/erg_'+instrument+'_'+level+'_'+datatype +'_%Y%m%d_'
-                if version == None:
-                    pathformat += 'v??_??.cdf'
-                else:
-                    pathformat += version + '.cdf'
-            if level == 'l3':
-                pathformat = 'satellite/erg/'+instrument+'/'+level+'/pa/%Y/%m/erg_'+instrument+'_'+level+'_pa_%Y%m%d_'
-                if version == None:
-                    pathformat += 'v??_??.cdf'
-                else:
-                    pathformat += version + '.cdf'
-
         elif instrument == 'orb':
             if level == 'l3':
                 if model == 'op':
