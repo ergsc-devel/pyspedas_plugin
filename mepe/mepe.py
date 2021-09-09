f8442e32846b2910f4e9202425f21e4f340a48ff
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-09 18:57:40 +0900

    Implemen pathformat, file_res, prefix from load.py, in mepe.py

--- mepe/mepe.py
+++ mepe/mepe.py
@@ -70,16 +70,21 @@ def mepe(trange=['2017-03-27', '2017-03-28'],
         List of tplot variables created.
 
     """
-    
+
+
     if level == 'l3':
         datatype = '3dflux'
 
-    loaded_data = load(instrument='mepe', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
+    file_res=3600. * 24
+    prefix = 'erg_mepe_'+level+'_'
+    pathformat = 'satellite/erg/mepe/'+level+'/'+datatype+'/%Y/%m/erg_mepe_'+level+'_'+datatype+'_%Y%m%d_v??_??.cdf'
+
+    loaded_data = load(pathformat=pathformat, trange=trange, level=level, datatype=datatype,file_res=file_res, prefix=prefix, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
     
     if len(loaded_data) > 0 and ror:
 
     
-        out_files = load(instrument='mepe', trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
+        out_files = load(pathformat=pathformat, trange=trange, level=level, datatype=datatype,file_res=file_res, prefix=prefix, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
         cdf_file = cdflib.CDF(out_files[0])
         gatt = cdf_file.globalattsget()
 
