d7b035ee50b17cb20199348d720ece1b5a83dd4c
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-09 13:36:46 +0900

    Add ror argument, its processing and documentation in pwe_hfa.py

--- pwe/pwe_hfa.py
+++ pwe/pwe_hfa.py
@@ -17,7 +17,8 @@ def pwe_hfa(trange=['2017-04-01', '2017-04-02'],
         no_update=False,
         uname=None,
         passwd=None,
-        time_clip=False):
+        time_clip=False,
+        ror=True):
     """
     This function loads data from the PWE experiment from the Arase mission
     
@@ -63,13 +64,16 @@ def pwe_hfa(trange=['2017-04-01', '2017-04-02'],
         time_clip: bool
             Time clip the variables to exactly the range specified in the trange keyword
 
+        ror: bool
+            If set, print PI info and rules of the road
+
     Returns:
         List of tplot variables created.
 
     """
     loaded_data = load(instrument='pwe_hfa', mode=mode, trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
     
-    if len(loaded_data) > 0:
+    if len(loaded_data) > 0 and ror:
 
     
         out_files = load(instrument='pwe_hfa', mode=mode, trange=trange, level=level, datatype=datatype, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=True, notplot=notplot, time_clip=time_clip, no_update=True, uname=uname, passwd=passwd)
