8a833e25335a5abb002be0328dfea680311be21c
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-10 20:06:37 +0900

    Modify index in lepe.py

--- lepe/lepe.py
+++ lepe/lepe.py
@@ -219,7 +219,7 @@ def lepe(trange=['2017-04-04', '2017-04-05'],
                 ytitle_eV_array = np.round(np.nan_to_num(FEDU_get_data[2][0, :]), 2)
 
                 if not only_fedu:
-                    for i in range(FEDU_get_data[2].shape[1]): # processing for erg_lepe_l3_pa_enech_??(??:01,01,..32)_FEDU
+                    for i in range(FEDU_get_data[1].shape[1]): # processing for erg_lepe_l3_pa_enech_??(??:01,01,..32)_FEDU
                         tplot_name = prefix + 'enech_' + str(i + 1).zfill(2)
                         store_data(tplot_name,data={'x':FEDU_get_data[0],
                                                 'y':FEDU_get_data[1][:,i,:],
