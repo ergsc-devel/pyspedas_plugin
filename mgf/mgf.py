27c5511928a0328ceaa5557900ee7e4b06746223
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-10 11:46:11 +0900

    Add clip processing for erg_mgf_l2_igrf_8sec_{dsi|gse|gsm|sm} Tplot Variables in mgf.py

--- mgf/mgf.py
+++ mgf/mgf.py
@@ -147,7 +147,11 @@ def mgf(trange=['2017-03-27', '2017-03-28'],
         
         clip(prefix + 'rmsd_'+datatype+suffix, 0., 80.)
         
-        
+        clip(prefix + 'igrf_'+datatype+'_dsi'+suffix,-1e+6, +1e+6)
+        clip(prefix + 'igrf_'+datatype+'_gse'+suffix, -1e+6, +1e+6)
+        clip(prefix + 'igrf_'+datatype+'_gsm'+suffix, -1e+6, +1e+6)
+        clip(prefix + 'igrf_'+datatype+'_sm'+suffix, -1e+6, +1e+6)
+
         # set yrange
         times, bdata = get_data(prefix + 'mag_'+datatype+'_dsi'+suffix)
         ylim(prefix + 'mag_'+datatype+'_dsi'+suffix, np.nanmin(bdata), np.nanmax(bdata))
