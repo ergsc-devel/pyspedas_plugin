8b283bbe556cbbc5946e5e9421454f6d018e6ed0
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-09-15 09:55:07 +0900

    Modify processing of lepi.py
    delete if sentence of notplot argument
    delete if sentence of type() is dict
    Rename original Tplot Variables which have negative values into '*_raw'
    Add processing to create Tplot Varialbes which have no negative values, by removing last two components from original ones
    Add flag_FPDO variable to avoid error of clip option
    Integrate return sentence for simplicity

--- lepi/lepi.py
+++ lepi/lepi.py
@@ -2,7 +2,7 @@
 #from pyspedas.erg.load import load
 from load import load
 import numpy as np
-from pytplot import options, clip, ylim, zlim, store_data
+from pytplot import options, clip, ylim, zlim, store_data, get_data
 import cdflib
 
 def lepi(trange=['2017-07-01', '2017-07-02'],
@@ -85,10 +85,6 @@ def lepi(trange=['2017-07-01', '2017-07-02'],
     else:
         pathformat += version + '.cdf'
 
-    if datatype == 'omniflux' and level == 'l2':
-        notplot=True # to avoid failure of creation plot variables (at store_data.py) of lepi
-    
-
     loaded_data = load(pathformat=pathformat, trange=trange, file_res=file_res, prefix=prefix, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
 
     if len(loaded_data) > 0 and ror:
@@ -117,84 +113,85 @@ def lepi(trange=['2017-07-01', '2017-07-02'],
         print('**************************************************************************')
 
 
-    if type(loaded_data) is dict:
 
-        if datatype == 'omniflux' and level == 'l2':
-            tplot_variables = []
+    if datatype == 'omniflux' and level == 'l2':
+        tplot_variables = []
 
-            if prefix + 'FPDO' + suffix in loaded_data:
-                v_array = loaded_data[prefix + 'FPDO' + suffix]['v']
-                v_array = np.where(v_array < 0. , np.nan, v_array) # change minus values to NaN
-                store_data(prefix + 'FPDO' + suffix, data={'x':loaded_data[prefix + 'FPDO' + suffix]['x'],
-                                                    'y':loaded_data[prefix + 'FPDO' + suffix]['y'],
-                                                    'v':v_array})
-                tplot_variables.append(prefix + 'FPDO' + suffix)
+        if prefix + 'FPDO' + suffix in loaded_data:
+            store_data(prefix + 'FPDO' + suffix,newname=prefix + 'FPDO_raw' + suffix)
+            loaded_data.append(prefix + 'FPDO_raw' + suffix)
+            get_data_vars = get_data(prefix + 'FPDO_raw' + suffix)
+            flag_FPDO = store_data(prefix + 'FPDO' + suffix, data={'x':get_data_vars[0],
+                                                                    'y':get_data_vars[1][:,:-2],
+                                                                    'v':get_data_vars[2][:-2]})
+            tplot_variables.append(prefix + 'FPDO' + suffix)
 
-            if prefix + 'FHEDO' + suffix in loaded_data:
-                v_array = loaded_data[prefix + 'FHEDO' + suffix]['v']
-                v_array = np.where(v_array < 0. , np.nan, v_array) # change minus values to NaN
-                store_data(prefix + 'FHEDO' + suffix, data={'x':loaded_data[prefix + 'FHEDO' + suffix]['x'],
-                                                    'y':loaded_data[prefix + 'FHEDO' + suffix]['y'],
-                                                    'v':v_array})
-                tplot_variables.append(prefix + 'FHEDO' + suffix)
 
-            if prefix + 'FODO' + suffix in loaded_data:
-                v_array = loaded_data[prefix + 'FODO' + suffix]['v']
-                v_array = np.where(v_array < 0. , np.nan, v_array) # change minus values to NaN
-                store_data(prefix + 'FODO' + suffix, data={'x':loaded_data[prefix + 'FODO' + suffix]['x'],
-                                                    'y':loaded_data[prefix + 'FODO' + suffix]['y'],
-                                                    'v':v_array})
-                tplot_variables.append(prefix + 'FODO' + suffix)
+        if prefix + 'FHEDO' + suffix in loaded_data:
+            store_data(prefix + 'FHEDO' + suffix,newname=prefix + 'FHEDO_raw' + suffix)
+            loaded_data.append(prefix + 'FHEDO_raw' + suffix)
+            get_data_vars = get_data(prefix + 'FHEDO_raw' + suffix)
+            store_data(prefix + 'FHEDO' + suffix, data={'x':get_data_vars[0],
+                                                        'y':get_data_vars[1][:,:-2],
+                                                        'v':get_data_vars[2][:-2]})
+            tplot_variables.append(prefix + 'FHEDO' + suffix)
 
-            # remove minus valuse of y array
-            if prefix + 'FPDO' + suffix in loaded_data:
-                clip(prefix + 'FPDO' + suffix, 0., 2.e+16)
-            
-            # set ytitle
-            options(tplot_variables, 'ytitle', 'LEPi\nomniflux\nLv2\nEnergy')
+        if prefix + 'FODO' + suffix in loaded_data:
+            store_data(prefix + 'FODO' + suffix,newname=prefix + 'FODO_raw' + suffix)
+            loaded_data.append(prefix + 'FODO_raw' + suffix)
+            get_data_vars = get_data(prefix + 'FODO_raw' + suffix)
+            store_data(prefix + 'FODO' + suffix, data={'x':get_data_vars[0],
+                                                        'y':get_data_vars[1][:,:-2],
+                                                        'v':get_data_vars[2][:-2]})
+            tplot_variables.append(prefix + 'FODO' + suffix)
 
-            # set ysubtitle
-            options(tplot_variables, 'ysubtitle', '[keV/q]')
-            
-            # set spectrogram plot option
-            options(tplot_variables, 'Spec', 1)
+        # remove minus valuse of y array
+        if flag_FPDO:
+            clip(prefix + 'FPDO' + suffix, 0., 2.e+16)
+        
+        # set ytitle
+        options(tplot_variables, 'ytitle', 'LEPi\nomniflux\nLv2\nEnergy')
 
-            # set y axis to logscale
-            options(tplot_variables, 'ylog', 1)
+        # set ysubtitle
+        options(tplot_variables, 'ysubtitle', '[keV/q]')
+        
+        # set spectrogram plot option
+        options(tplot_variables, 'Spec', 1)
 
-            for i in range(len(tplot_variables)):
-                # set ylim
-                ylim(tplot_variables[i], 0.01, 25.0)
-                # set zlim
-                zlim(tplot_variables[i], 1e+1, 1e+9)
+        # set y axis to logscale
+        options(tplot_variables, 'ylog', 1)
 
-            # set yrange
-            #options(prefix + 'FPDO' + suffix, 'yrange', [0.01, 20.])
-            #options(prefix + 'FHEDO' + suffix, 'yrange', [0.01, 20.])
-            #options(prefix + 'FODO' + suffix, 'yrange', [0.01, 20.])
+        for i in range(len(tplot_variables)):
+            # set ylim
+            ylim(tplot_variables[i], 0.01, 25.0)
+            # set zlim
+            zlim(tplot_variables[i], 1e+1, 1e+9)
 
-            # set ztitle
-            options(tplot_variables, 'ztitle', '[/cm^2-str-s-keV]')
+        # set yrange
+        #options(prefix + 'FPDO' + suffix, 'yrange', [0.01, 20.])
+        #options(prefix + 'FHEDO' + suffix, 'yrange', [0.01, 20.])
+        #options(prefix + 'FODO' + suffix, 'yrange', [0.01, 20.])
 
-            # set z axis to logscale
-            options(tplot_variables, 'zlog', 1)
+        # set ztitle
+        options(tplot_variables, 'ztitle', '[/cm^2-str-s-keV]')
 
-            # set zrange
-            #options(prefix + 'FPDO' + suffix, 'zrange', [1.e+02, 1.e+09])
-            #options(prefix + 'FHEDO' + suffix, 'zrange', [1.e+02, 1.e+09])
-            #options(prefix + 'FODO' + suffix, 'zrange', [1.e+01, 1.e+08])
+        # set z axis to logscale
+        options(tplot_variables, 'zlog', 1)
 
-            # change colormap option
-            options(tplot_variables, 'Colormap', 'jet')
+        # set zrange
+        #options(prefix + 'FPDO' + suffix, 'zrange', [1.e+02, 1.e+09])
+        #options(prefix + 'FHEDO' + suffix, 'zrange', [1.e+02, 1.e+09])
+        #options(prefix + 'FODO' + suffix, 'zrange', [1.e+01, 1.e+08])
 
-            return tplot_variables
+        # change colormap option
+        options(tplot_variables, 'Colormap', 'jet')
 
-        elif datatype == '3dflux' and level == 'l2':
-            if prefix + 'FPDU' + suffix in loaded_data:
-                clip(prefix + 'FPDU' + suffix, -1.0e+10, 1.0e+10)
-            if prefix + 'FHEDU' + suffix in loaded_data:
-                clip(prefix + 'FHEDU' + suffix, -1.0e+10, 1.0e+10)
-            if prefix + 'FODU' + suffix in loaded_data:
-                clip(prefix + 'FODU' + suffix, -1.0e+10, 1.0e+10)
+    elif datatype == '3dflux' and level == 'l2':
+        if prefix + 'FPDU' + suffix in loaded_data:
+            clip(prefix + 'FPDU' + suffix, -1.0e+10, 1.0e+10)
+        if prefix + 'FHEDU' + suffix in loaded_data:
+            clip(prefix + 'FHEDU' + suffix, -1.0e+10, 1.0e+10)
+        if prefix + 'FODU' + suffix in loaded_data:
+            clip(prefix + 'FODU' + suffix, -1.0e+10, 1.0e+10)
 
     return loaded_data
