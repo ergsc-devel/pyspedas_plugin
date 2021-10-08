Modify dtor variable, in order to avoid mistake in computation which use Trigonometric function, in erg_interpolate_att.py

common/cotrans/erg_interpolate_att.py
@@ -17,7 +17,7 @@ def erg_interpolate_att(erg_xxx_in = None):
     time = get_data(erg_xxx_in)[0]
 
     # Prepare some constants
-    dtor = np.pi
+    dtor = np.pi / 180.
 
     output_dictionary = {}
 