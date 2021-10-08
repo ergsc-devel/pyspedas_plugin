6044d8c6197cdf8f235502183194263b51a7fc45
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-08 10:05:54 +0900

    Update erg_interpolate_att.py
    Add processings for loading the attituede data, in order to prepare later analyzing.
    Add [0] of get_data()[0] in order to avoid attribute error in time.min() or max()

--- common/cotrans/erg_interpolate_att.py
+++ common/cotrans/erg_interpolate_att.py
@@ -2,7 +2,7 @@ import numpy as np
 #import pyspedas
 from pyspedas import tnames
 #import pytplot
-from pytplot import get_data
+from pytplot import get_data, get_timespan
 from pytplot.tplot_math.degap import degap
 from att.att import att
 from pyspedas.utilities.time_string import time_string
@@ -14,8 +14,19 @@ def erg_interpolate_att(erg_xxx_in = None):
         print(f'{erg_xxx_in} is None, or not defined')
         return
 
-    time = get_data(erg_xxx_in)
+    time = get_data(erg_xxx_in)[0]
 
     # Prepare some constants
     dtor = np.pi
 
+    
+    #Load the attitude data 
+    if tnames('erg_att_sprate') == ['erg_att_sprate']:
+        degap('erg_att_sprate',dt=8., margin=.5)
+        sprate=get_data('erg_att_sprate')
+        if sprate[0].min() > time.min() + 8. or sprate[0].max() < time.max() - 8.:
+            tr = get_timespan(erg_xxx_in)
+            att(trange=[time_string([tr[0] -60., tr[1] + 60.])])
+    else:
+        tr = get_timespan(erg_xxx_in)
+        att(trange=time_string([tr[0] -60., tr[1] + 60.]))
