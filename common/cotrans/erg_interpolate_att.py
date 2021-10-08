fefec2e9c47307cc30ab1f60d596b6c4e3f24c42
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-08 09:50:33 +0900

    Update erg_interpolate_att.py
    Change some import statement, in order to simplify.
    Add processing some initial values, in order to prepare processings.

--- common/cotrans/erg_interpolate_att.py
+++ common/cotrans/erg_interpolate_att.py
@@ -1,6 +1,8 @@
 import numpy as np
-import pyspedas
-import pytplot
+#import pyspedas
+from pyspedas import tnames
+#import pytplot
+from pytplot import get_data
 from pytplot.tplot_math.degap import degap
 from att.att import att
 from pyspedas.utilities.time_string import time_string
@@ -8,6 +10,12 @@ from pyspedas.utilities.time_string import time_string
 
 def erg_interpolate_att(erg_xxx_in = None):
 
-    if erg_xxx_in == None or erg_xxx_in not in pyspedas.tnames():
+    if erg_xxx_in == None or erg_xxx_in not in tnames():
         print(f'{erg_xxx_in} is None, or not defined')
         return
+
+    time = get_data(erg_xxx_in)
+
+    # Prepare some constants
+    dtor = np.pi
+
