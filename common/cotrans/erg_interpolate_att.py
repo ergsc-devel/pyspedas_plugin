781335da802d5e85b007e77f4c7e0af9486f8f41
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-08 09:44:40 +0900

    Update erg_interpolate_att.py
    Add def statement in order to define the function of erg_interpolate_att.
    Add processing about input of erg_xxx_in variaable, in order to avoid error.

--- common/cotrans/erg_interpolate_att.py
+++ common/cotrans/erg_interpolate_att.py
@@ -4,3 +4,10 @@ import pytplot
 from pytplot.tplot_math.degap import degap
 from att.att import att
 from pyspedas.utilities.time_string import time_string
+
+
+def erg_interpolate_att(erg_xxx_in = None):
+
+    if erg_xxx_in == None or erg_xxx_in not in pyspedas.tnames():
+        print(f'{erg_xxx_in} is None, or not defined')
+        return
