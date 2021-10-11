810e922e9d6b1e973461cdbb17259cb5083be49f
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-11 14:35:34 +0900

    Update dsi2j2000.py
    Change indentation in order to readability.
    Add get_data processing, in order to prepare for transformed Tplot Variable.
    Add no_orb argument in order to option for not orbit using.

--- common/cotrans/dsi2j2000.py
+++ common/cotrans/dsi2j2000.py
@@ -6,16 +6,35 @@ from common.cotrans.cart_trans_matrix_make import cart_trans_matrix_make
 from pyspedas.utilities.time_string import time_string
 from pyspedas.cotrans.cotrans import cotrans
 from pyspedas.analysis.tcrossp import tcrossp
+import numpy as np
 
 def dsi2j2000(name_in=None,
-              name_out=None):
+              name_out=None,
+              no_orb=False):
 
-              if name_in == None or name_in not in tplot_names(quiet=True):
-                  print('Input of Tplot name is undifiend')
-                  return
+            if name_in == None or name_in not in tplot_names(quiet=True):
+                print('Input of Tplot name is undifiend')
+                return
 
-              if name_out == None:
-                  print('Tplot name for output is undifiend')
-                  name_out = 'result_of_dsi2j2000'
+            if name_out == None:
+                print('Tplot name for output is undifiend')
+                name_out = 'result_of_dsi2j2000'
 
+            # prepare for transformed Tplot Variable
+            dl_in = get_data(name_in, metadata=True)
+            get_data_array = get_data(name_in)
+            time = get_data_array[0]
+            time_length = time.shape[0]
+            dat = get_data_array[1]
+
+
+            #Get the SGI axis by interpolating the attitude data
+            dsiz_j2000 = erg_interpolate_att(name_in)['sgiz_j2000']
+
+
+            #Sun direction in J2000
+            sundir=np.array([[1., 0., 0.]]*time_length)
+
+            if no_orb:
+                store_data('sundir_gse',data={'x':time, 'y':sundir})
 
