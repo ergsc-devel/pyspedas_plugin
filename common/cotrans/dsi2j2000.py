84603d77dcf1f0bf8a827442770cdd52a0bd9560
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-11 14:22:14 +0900

    Add processing for check of name_out argument, in order to avoid error, in dsi2j2000.py

--- common/cotrans/dsi2j2000.py
+++ common/cotrans/dsi2j2000.py
@@ -7,11 +7,15 @@ from pyspedas.utilities.time_string import time_string
 from pyspedas.cotrans.cotrans import cotrans
 from pyspedas.analysis.tcrossp import tcrossp
 
-def dsi2j2000(name_in=None
-              ):
+def dsi2j2000(name_in=None,
+              name_out=None):
 
               if name_in == None or name_in not in tplot_names(quiet=True):
                   print('Input of Tplot name is undifiend')
                   return
 
-            
+              if name_out == None:
+                  print('Tplot name for output is undifiend')
+                  name_out = 'result_of_dsi2j2000'
+
+
