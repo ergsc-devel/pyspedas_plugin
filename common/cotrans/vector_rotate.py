666053ed1320b6b81ea90f191505222c6114fd45
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 14:04:29 +0900

    Update vector_rotate.py
    Add nomalization  the rotation axis vector to the unit vector,  in order to use later rodrigues matrix.

--- common/cotrans/vector_rotate.py
+++ common/cotrans/vector_rotate.py
@@ -7,3 +7,9 @@ def vector_rotate( x0, y0, z0, nx, ny, nz, theta, x1, y1, z1):
     the = theta * dtor
     costhe = np.cos(the)
     sinthe = np.sin(the)
+
+    #Normalize the rotation axis vector to the unit vector
+    n = np.sqrt(nx*nx + ny*ny + nz*nz)
+    nx /= n
+    ny /= n
+    nz /= n
