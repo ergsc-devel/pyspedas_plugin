0a28a2468121e040241845bd0d98fb2b41f864e4
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 14:08:58 +0900

    Update vector_rotate.py
    Add x0_length variable in order to prepare to specify the array length.
    Add rodrigues_mat, in order to prepare rotation matrix and computation which use its matrix.

--- common/cotrans/vector_rotate.py
+++ common/cotrans/vector_rotate.py
@@ -7,9 +7,16 @@ def vector_rotate( x0, y0, z0, nx, ny, nz, theta, x1, y1, z1):
     the = theta * dtor
     costhe = np.cos(the)
     sinthe = np.sin(the)
+    x0_length = x0.shape[0]
 
     #Normalize the rotation axis vector to the unit vector
     n = np.sqrt(nx*nx + ny*ny + nz*nz)
     nx /= n
     ny /= n
     nz /= n
+
+    rodrigues_mat =  np.concatenate([
+    np.array([ nx*nx*(1. -costhe) + costhe, nx*ny*(1. -costhe)-nz*sinthe, nz*nx*(1. -costhe)+ny*sinthe ]).T,
+    np.array([ nx*ny*(1. -costhe)+nz*sinthe, ny*ny*(1. -costhe)+costhe, ny*nz*(1. -costhe)-nx*sinthe ]).T,
+    np.array([nz*nx*(1. -costhe)-ny*sinthe, ny*nz*(1. -costhe)+nx*sinthe, nz*nz*(1. -costhe)+costhe ]).T 
+    ],axis=1).reshape(x0_length,3,3)
