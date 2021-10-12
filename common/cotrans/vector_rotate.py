d45d5d75d5e3bdbc41b695c1f247760e2a42518e
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 14:13:42 +0900

    Delete extra argument, in order to avoid error, in vector_rotate.py

--- common/cotrans/vector_rotate.py
+++ common/cotrans/vector_rotate.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def vector_rotate( x0, y0, z0, nx, ny, nz, theta, x1, y1, z1):
+def vector_rotate( x0, y0, z0, nx, ny, nz, theta):
 
     # Prepare sin\cos values for the rotation angle theta.
     dtor = np.pi / 180. # deg --> rad
