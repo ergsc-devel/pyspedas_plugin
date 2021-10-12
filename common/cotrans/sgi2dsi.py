b870309a1f0a8f86316d02fad3e7af78161d92df
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 16:50:30 +0900

    Update sgi2dsi.py
    Delete no use argument, in order to avoid confusion.

--- common/cotrans/sgi2dsi.py
+++ common/cotrans/sgi2dsi.py
@@ -4,7 +4,6 @@ from common.cotrans.erg_interpolate_att import erg_interpolate_att
 
 def sgi2dsi(name_in=None,
             name_out=None,
-            no_orb=False,
             DSI2SGI=False,
             noload=False):
 
