999d10cf62eb4b6f0b3b3d2d4eac4c1d8b1a319d
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 16:47:27 +0900

    Update sgi2dsi.py
    Add def statement in order to create sgi2dsi function.
    Add argument of sgi2dsi function in order to prepare computation which use these values.

--- common/cotrans/sgi2dsi.py
+++ common/cotrans/sgi2dsi.py
@@ -2,3 +2,9 @@ import numpy as np
 from pytplot import get_data
 from common.cotrans.erg_interpolate_att import erg_interpolate_att
 
+def sgi2dsi(name_in=None,
+            name_out=None,
+            no_orb=False,
+            DSI2SGI=False,
+            noload=False):
+            
