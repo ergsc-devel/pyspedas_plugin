0ea4bf6d31ca331c95ad78c2d6b99dba64d4e86b
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 16:11:25 +0900

    Update vector_rotate.py
    Add IF sentences about length and concatenate_axis, and IF sentences about dot product for rotated vector, in order to adapt below three conditions.
    1. For (x0, y0, z0) and (nx,ny,nz) given as a single vector
    2. For all of (x0, y0, z0), (nx, ny, nz), theta given as arrays.
    3.For (x0,y0,z0) given as an array with a vector nx and scalar theta.

--- common/cotrans/vector_rotate.py
+++ common/cotrans/vector_rotate.py
@@ -7,7 +7,19 @@ def vector_rotate( x0, y0, z0, nx, ny, nz, theta):
     the = theta * dtor
     costhe = np.cos(the)
     sinthe = np.sin(the)
-    x0_length = x0.shape[0]
+    if isinstance(x0, np.ndarray): # for array x0
+        x0_length = x0.shape[0]
+        concatenate_axis_x0 = 1
+    else: # for vector x0 (single component)
+        x0_length = 1
+        concatenate_axis_x0 = 0
+    
+    if isinstance(nx, np.ndarray): # for array nx
+        nx_length = nx.shape[0]
+        concatenate_axis_n = 1
+    else: # for vector nx (single component)
+        nx_length = 1
+        concatenate_axis_n = 0
 
     #Normalize the rotation axis vector to the unit vector
     n = np.sqrt(nx*nx + ny*ny + nz*nz)
@@ -19,9 +31,14 @@ def vector_rotate( x0, y0, z0, nx, ny, nz, theta):
     np.array([ nx*nx*(1. -costhe) + costhe, nx*ny*(1. -costhe)-nz*sinthe, nz*nx*(1. -costhe)+ny*sinthe ]).T,
     np.array([ nx*ny*(1. -costhe)+nz*sinthe, ny*ny*(1. -costhe)+costhe, ny*nz*(1. -costhe)-nx*sinthe ]).T,
     np.array([nz*nx*(1. -costhe)-ny*sinthe, ny*nz*(1. -costhe)+nx*sinthe, nz*nz*(1. -costhe)+costhe ]).T 
-    ],axis=1).reshape(x0_length,3,3)
+    ],axis=concatenate_axis_n).reshape(nx_length,3,3)
 
-    inputed_vector =np.concatenate([[x0,y0,z0]],axis=1).T
-    rotated_vector = [np.dot(rodrigues_mat[i,:,:], inputed_vector[i,:]) for i in range(x0_length)]
+    inputed_vector =np.concatenate([[x0,y0,z0]],axis=concatenate_axis_x0).T
+    if x0_length > 1 and nx_length > 1: #For all of (x0, y0, z0), (nx, ny, nz), (and theta) given as arrays. 
+        rotated_vector = np.array([np.dot(rodrigues_mat[i,:,:], inputed_vector[i,:]) for i in range(x0_length)])
+    elif x0_length > 1 and nx_length == 1: # For (x0,y0,z0) given as an array with a vector nx and scalar theta.
+        rotated_vector = np.dot(rodrigues_mat, inputed_vector.T).T.reshape(x0_length, 3)
+    elif x0_length == 1 and nx_length == 1: #For (x0, y0, z0) and (nx,ny,nz) given as a single vector
+        rotated_vector = np.dot(rodrigues_mat, inputed_vector).reshape(3)
 
     return rotated_vector
