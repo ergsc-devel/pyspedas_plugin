ec408260b0d1b95be30601b261ed5129c7c130b4
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-12 16:50:06 +0900

    Update sgi2dsi.py
    Add IF statement about name_in and name_out argument, in order to avoid error about these.

--- common/cotrans/sgi2dsi.py
+++ common/cotrans/sgi2dsi.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pytplot import get_data
+from pytplot import get_data, tplot_names
 from common.cotrans.erg_interpolate_att import erg_interpolate_att
 
 def sgi2dsi(name_in=None,
@@ -7,4 +7,11 @@ def sgi2dsi(name_in=None,
             no_orb=False,
             DSI2SGI=False,
             noload=False):
-            
+
+            if name_in == None or name_in not in tplot_names(quiet=True):
+                print('Input of Tplot name is undifiend')
+                return
+
+            if name_out == None:
+                print('Tplot name for output is undifiend')
+                name_out = 'result_of_dsi2j2000'
