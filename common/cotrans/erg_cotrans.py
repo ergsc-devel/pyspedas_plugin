a82bbf416e39336bb65f1f1411623b61cb92a764
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-13 17:07:00 +0900

    Add transformation DSI->SGA in erg_coord_trans() of erg_cotrans.py
    In order to implement transformation dsi --> sgi --> sga.

--- common/cotrans/erg_cotrans.py
+++ common/cotrans/erg_cotrans.py
@@ -1,4 +1,6 @@
 from pytplot import tplot_copy
+from common.cotrans.sgi2dsi import sgi2dsi
+from common.cotrans.sga2sgi import sga2sgi
 
 def erg_replace_coord_suffix(in_name=None, out_coord=None):
     nms = in_name.split('_')
@@ -15,3 +17,10 @@ def erg_coord_trans(in_name=None,
                         tplot_copy(in_name, out_name)
                         return
                         
+                    #From DSI
+                    if in_coord == 'dsi':
+
+                        if out_coord == 'sga': # dsi --> sgi --> sga
+                            name_sgi = erg_replace_coord_suffix(in_name=in_name, out_coord='sgi')
+                            sgi2dsi(name_in=in_name, name_out=name_sgi, DSI2SGI=True, noload=noload)
+                            sga2sgi(name_in=name_sgi, name_out=out_name, SGI2SGA=True, noload=noload)
