4da48916293eaabee2e9aa361556b93d8123f7e1
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-13 16:57:03 +0900

    Add processing for same in_coord and out_coord, in erg_cotrans.py
    In order to avoid extra processing.

--- common/cotrans/erg_cotrans.py
+++ common/cotrans/erg_cotrans.py
@@ -1,3 +1,5 @@
+from pytplot import tplot_copy
+
 def erg_replace_coord_suffix(in_name=None, out_coord=None):
     nms = in_name.split('_')
     nms[-1] = out_coord
@@ -9,3 +11,7 @@ def erg_coord_trans(in_name=None,
                     out_coord=None,
                     noload=False):
 
+                    if in_coord == out_coord:
+                        tplot_copy(in_name, out_name)
+                        return
+                        
