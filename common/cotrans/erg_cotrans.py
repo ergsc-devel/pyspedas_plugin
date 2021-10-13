5bfa9e77bcbf79cb60481fde592a0dbf6402ea95
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-13 16:51:01 +0900

    Add erg_coord_trans() and its argument, in erg_cotrans.py
    In order to prepare implementation of pro erg_coord_trans in erg_cotrans.pro.

--- common/cotrans/erg_cotrans.py
+++ common/cotrans/erg_cotrans.py
@@ -2,3 +2,10 @@ def erg_replace_coord_suffix(in_name=None, out_coord=None):
     nms = in_name.split('_')
     nms[-1] = out_coord
     return '_'.join(nms)
+
+def erg_coord_trans(in_name=None,
+                    out_name=None,
+                    in_coord=None,
+                    out_coord=None,
+                    noload=False):
+
