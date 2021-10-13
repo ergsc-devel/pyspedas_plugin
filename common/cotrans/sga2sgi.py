b4fb08410655c6910ac9f16c39b4936c622f1ed1
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-13 10:56:02 +0900

    Add def statement and some arguments, in sga2sgi.py
    In order to prepare for implementation of sga2sgi.pro

--- common/cotrans/sga2sgi.py
+++ common/cotrans/sga2sgi.py
@@ -2,3 +2,8 @@ import numpy as np
 from pytplot import get_data, tplot_names, store_data
 from common.cotrans.erg_interpolate_att import erg_interpolate_att
 
+def sga2sgi(name_in=None,
+            name_out=None,
+            SGI2SGA=False,
+            noload=False):
+
