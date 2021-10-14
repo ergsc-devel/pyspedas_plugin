1bf83112757547d8f3218eb22c1cf839227fad21
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-14 10:30:24 +0900

    Add FOR statement about in_names, in erg_cotrans.py.
    In order to prepare to use erg_coord_trans().

--- common/cotrans/erg_cotrans.py
+++ common/cotrans/erg_cotrans.py
@@ -39,3 +39,22 @@ def erg_cotrans(in_name='',
                 if len(in_names) == 0:
                     print('in_name is not match in stored Tplot Variables, return')
                     return
+
+                for input_name in in_names:
+                    if in_coord not in valid_suffixes:
+                        if (input_name.split('_')[-1] in valid_suffixes):
+                            in_suf = input_name.split('_')[-1]
+                        else:
+                            print(f'Cannot get a valid coord. suffix of input variable for {input_name}.')
+                            continue
+                    else:
+                        in_suf = in_coord
+                    if out_coord not in valid_suffixes:
+                        if out_name.split('_')[-1] in valid_suffixes:
+                            out_suf = out_name.split('_')[-1]
+                        else:
+                            print(f'Cannot get a valid coord. suffix of output variable for {input_name}.')
+                            continue
+                    else:
+                        out_suf = out_coord
+                    
