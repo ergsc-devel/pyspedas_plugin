4e007369320e38c0345b5256b142265fcefd5cbe
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-10-18 15:10:31 +0900

    Add processing for waveform Tplot Variables, in pwe_wfc.py.
    In order to adjust element arrays shapes.

--- pwe/pwe_wfc.py
+++ pwe/pwe_wfc.py
@@ -1,7 +1,8 @@
 
 #from pyspedas.erg.load import load
+import numpy as np
 from load import load
-from pytplot import options, clip, ylim, zlim
+from pytplot import options, clip, ylim, zlim, get_data, store_data
 from pyspedas import tnames
 import cdflib
 
@@ -83,8 +84,7 @@ def pwe_wfc(trange=['2017-04-01/12:00:00', '2017-04-01/13:00:00'],
     loaded_data = []
     if level == 'l2':
         if datatype == 'waveform':
-            prefix_list = []
-            component_suffix_list = []
+            tplot_name_list = []
             if component == 'all':
                 component_list =['e', 'b']
             elif component == 'e' or component == 'b':
@@ -93,6 +93,10 @@ def pwe_wfc(trange=['2017-04-01/12:00:00', '2017-04-01/13:00:00'],
                 prefix = 'erg_pwe_wfc_' + level + '_' + com + '_' + mode +'_'
                 pathformat = 'satellite/erg/pwe/wfc/'+level+'/'+datatype+'/%Y/%m/erg_pwe_wfc_'+level+'_'+com+'_'+datatype+'_'+mode+'_'+coord+'_%Y%m%d%H_v??_??.cdf'
                 loaded_data +=load(pathformat=pathformat, trange=trange, level=level, datatype=datatype,file_res=file_res, prefix=prefix, suffix=suffix, get_support_data=get_support_data, varformat=varformat, varnames=varnames, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
+                if com == 'e':
+                    tplot_name_list += [prefix + 'Ex_waveform', prefix + 'Ey_waveform']
+                elif com == 'b':
+                    tplot_name_list += [prefix + 'Bx_waveform',prefix + 'By_waveform', prefix + 'Bz_waveform']
         elif datatype == 'spec':
             prefix_list = []
             component_suffix_list = []
@@ -136,7 +140,6 @@ def pwe_wfc(trange=['2017-04-01/12:00:00', '2017-04-01/13:00:00'],
         except:
             print('printing PI info and rules of the road was failed')
 
-    tplot_names_list = []
     if datatype == 'spec':
         for i in range(len(prefix_list)):
             options(prefix_list[i] + component_suffix_list[i], 'spec', 1)
@@ -153,5 +156,18 @@ def pwe_wfc(trange=['2017-04-01/12:00:00', '2017-04-01/13:00:00'],
                 zlim(prefix_list[i] + component_suffix_list[i], 1e-4, 1e2)
                 options(prefix_list[i] + component_suffix_list[i], 'ztitle', '[pT^2/Hz]')
                 options(prefix_list[i] + component_suffix_list[i], 'ytitle', 'B\nspectra')
+
+    if datatype == 'waveform':
+        for t_plot_name in tplot_name_list:
+            get_data_vars = get_data(t_plot_name)
+            dl_in = get_data(t_plot_name, metadata=True)
+            time1 = get_data_vars[0]
+            data = np.where(get_data_vars[1] <= -1e+30, np.nan, get_data_vars[1])
+            dt = get_data_vars[2]
+            ndt=dt.size
+            ndata=data.size
+            time_new = (np.tile(time1, (ndt, 1)).T + dt * 1e+6).reshape(ndata)
+            data_new = data.reshape(ndata)
+            store_data(t_plot_name,data={'x':time_new, 'y':data_new}, attr_dict=dl_in)
 
     return loaded_data
