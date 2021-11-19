fcf27253df0e1df391980c646527870ba13a4423
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-11-19 17:04:10 +0900

    Formatted get_mepe_flux_angle_in_sga.py.
    In order to improve readability.

--- ergpyspedas/erg/satellite/erg/particle/get_mepe_flux_angle_in_sga.py
+++ ergpyspedas/erg/satellite/erg/particle/get_mepe_flux_angle_in_sga.py
@@ -4,6 +4,7 @@ from astropy.coordinates import cartesian_to_spherical
 
 from .get_mepe_az_dir_in_sga import get_mepe_az_dir_in_sga
 
+
 def get_mepe_flux_angle_in_sga(looking_dir=False):
 
     fluxdir = looking_dir
@@ -11,16 +12,16 @@ def get_mepe_flux_angle_in_sga(looking_dir=False):
     sgajdir = get_mepe_az_dir_in_sga(fluxdir=fluxdir)
 
     spherical_data = cartesian_to_spherical(
-                                x=sgajdir[0],
-                                y=sgajdir[1],
-                                z=sgajdir[2]
-                                )
+        x=sgajdir[0],
+        y=sgajdir[1],
+        z=sgajdir[2]
+    )
 
-    rtod =  180. / np.pi
+    rtod = 180. / np.pi
     elev_array = spherical_data[1].value * rtod
     phi_array = spherical_data[2].value * rtod
 
-    anglarr = np.zeros(shape=( 2, 3, 16 ))  # ;[ elev/phi, min/cnt/max, apd_no ]
+    anglarr = np.zeros(shape=(2, 3, 16))  # ;[ elev/phi, min/cnt/max, apd_no ]
 
     for ch in range(15):
         anglarr[0, :, ch] = elev_array[ch]
