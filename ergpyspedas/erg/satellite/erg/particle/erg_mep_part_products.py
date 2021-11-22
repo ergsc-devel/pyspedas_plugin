146ebb0fce85c0cb2d19a8ecd2cc50d749cc0999
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-11-22 14:19:31 +0900

    Add processing for energy output in erg_mep_part_products.py.
    In order to be able to get energy spectrogram plot.

--- ergpyspedas/erg/satellite/erg/particle/erg_mep_part_products.py
+++ ergpyspedas/erg/satellite/erg/particle/erg_mep_part_products.py
@@ -5,6 +5,7 @@ from pyspedas.utilities.time_string import time_string
 from pyspedas.particles.spd_part_products.spd_pgs_make_e_spec import spd_pgs_make_e_spec
 from pyspedas.particles.spd_part_products.spd_pgs_make_theta_spec import spd_pgs_make_theta_spec
 from pyspedas.particles.spd_part_products.spd_pgs_progress_update import spd_pgs_progress_update
+from pyspedas.particles.spd_part_products.spd_pgs_make_tplot import spd_pgs_make_tplot
 from pytplot import get_timespan, get_data
 
 from .erg_mepe_get_dist import erg_mepe_get_dist
@@ -26,10 +27,12 @@ def erg_mep_part_products(
     theta=[-90., 90.],
     phi_in=[0., 360.],
     gyro=[0., 360.],
+    energy=None,
     fac_type='mphism',
     trange=None,
     mag_name=None,
-    pos_name=None
+    pos_name=None,
+    relativistic=False
     ):
 
     if len(tnames(in_tvarname)) < 1:
@@ -63,3 +66,51 @@ def erg_mep_part_products(
         
         if time_indices.shape[0] < 1:
             print(f'No ,{in_tvarname}, data for time range ,{time_string(trange_double)}')
+
+    elif trange is None:
+        time_indices = np.arange(times_array.shape[0])
+
+    times_array = times_array[time_indices]
+
+
+
+    dist = erg_mepe_get_dist(in_tvarname, 0, species=species, units=units_lc)
+    if 'energy' in outputs_lc:
+        out_energy = np.zeros((times_array.shape[0], dist['n_energy']))
+        out_energy_y = np.zeros((times_array.shape[0], dist['n_energy']))
+    if 'theta' in outputs_lc:
+        out_theta = np.zeros((times_array.shape[0], dist['n_theta']))
+        out_theta_y = np.zeros((times_array.shape[0], dist['n_theta']))
+
+    last_update_time = None
+    """
+    ;;-------------------------------------------------
+    ;; Loop over time to build spectrograms and/or moments
+    ;;-------------------------------------------------
+    """
+    for index in range(time_indices.shape[0]):
+
+        last_update_time = spd_pgs_progress_update(last_update_time=last_update_time,
+             current_sample=index, total_samples=time_indices.shape[0], type_string=in_tvarname)
+
+        #  ;; Get the data structure for this sample
+
+        if instnm == 'mepe':
+
+            dist = erg_mepe_get_dist(in_tvarname, time_indices[index], species=species, units=units_lc)
+
+        clean_data = erg_pgs_clean_data(dist, units=units_lc,relativistic=relativistic)
+        limited_data = erg_pgs_limit_range(clean_data, phi=phi_in, theta=theta, energy=energy)
+
+
+        #  ;;Build theta spectrogram
+        if 'theta' in outputs_lc:
+            out_theta_y[index, :], out_theta[index, :] = spd_pgs_make_theta_spec(limited_data)
+
+        #  ;;Build energy spectrogram
+        if 'energy' in outputs_lc:
+            out_energy_y[index, :], out_energy[index, :] = spd_pgs_make_e_spec(limited_data)
+
+    if 'energy' in outputs_lc:
+        spd_pgs_make_tplot(in_tvarname+'_energy' + suffix, x=times_array, y=out_energy_y, z=out_energy, units=units, ylog=True, ytitle=dist['data_name'] + ' \\ energy (eV)')
+
