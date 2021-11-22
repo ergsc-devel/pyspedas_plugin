58b5de37fe7f02655c60ae6d6ac788b0493effac
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-11-22 13:36:56 +0900

    Add processing for trange, and fix typo, in erg_mep_part_products.py.
    In order to avoid errors.

--- ergpyspedas/erg/satellite/erg/particle/erg_mep_part_products.py
+++ ergpyspedas/erg/satellite/erg/particle/erg_mep_part_products.py
@@ -1,10 +1,13 @@
 import numpy as np
 from pyspedas import tnames
+from pyspedas.utilities.time_double import time_double
+from pyspedas.utilities.time_string import time_string
 from pyspedas.particles.spd_part_products.spd_pgs_make_e_spec import spd_pgs_make_e_spec
 from pyspedas.particles.spd_part_products.spd_pgs_make_theta_spec import spd_pgs_make_theta_spec
 from pyspedas.particles.spd_part_products.spd_pgs_progress_update import spd_pgs_progress_update
-from pytplot import get_data
+from pytplot import get_timespan, get_data
 
+from .erg_mepe_get_dist import erg_mepe_get_dist
 from .erg_pgs_clean_data import erg_pgs_clean_data
 from .erg_pgs_limit_range import erg_pgs_limit_range
 from .erg_convert_flux_units import erg_convert_flux_units
@@ -12,7 +15,7 @@ from .erg_convert_flux_units import erg_convert_flux_units
 
 def erg_mep_part_products(
     in_tvarname,
-    spiecies=None,
+    species=None,
     outputs=['energy'],
     no_ang_weighting=False,
     suffix='',
@@ -43,3 +46,20 @@ def erg_mep_part_products(
         outputs_lc = []
         for output in outputs:
             outputs_lc.append(output.lower())
+
+    units_lc = units.lower()
+
+    #  ;;Preserve the original time range
+    tr_org = get_timespan(in_tvarname)
+
+    if instnm == 'mepe':
+        times_array = erg_mepe_get_dist(in_tvarname, species=species, units=units_lc, time_only=True)
+
+    if trange is not None:
+        
+        trange_double = time_double(trange)
+        time_indices = np.where((times_array >= trange_double[0]) \
+                                & (times_array <= trange_double[1]))[0]
+        
+        if time_indices.shape[0] < 1:
+            print(f'No ,{in_tvarname}, data for time range ,{time_string(trange_double)}')
