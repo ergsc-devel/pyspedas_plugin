ddba54dbb176c45d7fc3c5baf52cce1f3ed6d62d
AARUESU <87346044+AARUESU@users.noreply.github.com>
2021-12-09 13:15:48 +0900

    Implement erg_pgs_make_phi_spec(), in erg_pgs_make_phi_spec.py.
    In order to be able to use no_ang_weighting.

--- ergpyspedas/erg/satellite/erg/particle/erg_pgs_make_phi_spec.py
+++ ergpyspedas/erg/satellite/erg/particle/erg_pgs_make_phi_spec.py
@@ -3,6 +3,8 @@ import math
 import numpy as np
 from scipy.ndimage.interpolation import shift
 
+from copy import deepcopy
+
 # use nansum from bottleneck if it's installed, otherwise use the numpy one
 try:
     import bottleneck as bn
@@ -10,7 +12,7 @@ try:
 except ImportError:
     nansum = np.nansum
 
-def erg_pgs_make_phi_spec(data_in, resolution=None):
+def erg_pgs_make_phi_spec(data_in, resolution=None, no_ang_weighting=False):
     """
     Builds phi (longitudinal) spectrogram from the particle data structure
 
@@ -28,13 +30,10 @@ def erg_pgs_make_phi_spec(data_in, resolution=None):
 
     dr = math.pi/180.
 
-    data = data_in.copy()
+    data = deepcopy(data_in)
 
     # zero inactive bins to ensure areas with no data are represented as NaN
-    zero_bins = np.argwhere(data['bins'] == 0)
-    if zero_bins.size != 0:
-        for item in zero_bins:
-            data['data'][item[0], item[1]] = 0.0
+    data['data'] = np.where(data['bins'] == 0,0.,data['data'])
 
     # get number of phi values
     if resolution is None:
@@ -55,6 +54,7 @@ def erg_pgs_make_phi_spec(data_in, resolution=None):
     # keep phi in [0, 360]
     phi_min = (data['phi'] - 0.5*data['dphi']) 
     phi_max = (data['phi'] + 0.5*data['dphi']) % 360.0
+    phi_array = deepcopy(data['phi'])
 
     # algorithm below assumes maximums at 360 not wrapped to 0
     phi_max[phi_max == 0] = 360.0
@@ -84,41 +84,53 @@ def erg_pgs_make_phi_spec(data_in, resolution=None):
     # overlap.  All overlapping bins will be weighted according to the solid 
     # angle of their intersection and averaged.
     for i in range(0, n_phi):
-        weight = np.zeros(phi_min_flat.shape)
 
-        # data bins whose maximum overlaps the current spectrogram bin
-        idx_max = np.argwhere((phi_max_flat > phi_grid[i]) & (phi_max_flat < phi_grid[i+1]))
-        if idx_max.size != 0:
-            weight[idx_max] = (phi_max_flat[idx_max] - phi_grid[i]) * omega_part_flat[idx_max]
+        if not no_ang_weighting:
+            weight = np.zeros(phi_min_flat.shape)
 
-        # data bins whose minimum overlaps the current spectrogram bin
-        idx_min = np.argwhere((phi_min_flat > phi_grid[i]) & (phi_min_flat < phi_grid[i+1]))
-        if idx_min.size != 0:
-            weight[idx_min] = (phi_grid[i+1] - phi_min_flat[idx_min]) * omega_part_flat[idx_min]
+            # data bins whose maximum overlaps the current spectrogram bin
+            idx_max = np.argwhere((phi_max_flat > phi_grid[i]) & (phi_max_flat < phi_grid[i+1]))
+            if idx_max.size != 0:
+                weight[idx_max] = (phi_max_flat[idx_max] - phi_grid[i]) * omega_part_flat[idx_max]
 
-        # data bins contained within the current spectrogram bin
-        contained = np.intersect1d(idx_max, idx_min)
-        if contained.size != 0:
-            weight[contained] = dphi_flat[contained] * omega_part_flat[contained]
-        
-        # data bins that completely cover the current spectrogram bin 
-        idx_all = np.argwhere(((phi_min_flat <= phi_grid[i]) & (phi_max_flat >= phi_grid[i+1])) |
-                              (wrapped_flat & ((phi_min_flat > phi_grid[i+1]) & (phi_max_flat > phi_grid[i+1]))) |
-                              (wrapped_flat & ((phi_min_flat < phi_grid[i]) & (phi_max_flat < phi_grid[i]))))
-        if idx_all.size != 0:
-            weight[idx_all] = phi_grid_width * omega_part_flat[idx_all]
+            # data bins whose minimum overlaps the current spectrogram bin
+            idx_min = np.argwhere((phi_min_flat > phi_grid[i]) & (phi_min_flat < phi_grid[i+1]))
+            if idx_min.size != 0:
+                weight[idx_min] = (phi_grid[i+1] - phi_min_flat[idx_min]) * omega_part_flat[idx_min]
 
-        # combine indices
-        idx = np.unique(np.concatenate((idx_min, idx_max, idx_all)))
+            # data bins contained within the current spectrogram bin
+            contained = np.intersect1d(idx_max, idx_min)
+            if contained.size != 0:
+                weight[contained] = dphi_flat[contained] * omega_part_flat[contained]
+            
+            # data bins that completely cover the current spectrogram bin 
+            idx_all = np.argwhere(((phi_min_flat <= phi_grid[i]) & (phi_max_flat >= phi_grid[i+1])) |
+                                (wrapped_flat & ((phi_min_flat > phi_grid[i+1]) & (phi_max_flat > phi_grid[i+1]))) |
+                                (wrapped_flat & ((phi_min_flat < phi_grid[i]) & (phi_max_flat < phi_grid[i]))))
+            if idx_all.size != 0:
+                weight[idx_all] = phi_grid_width * omega_part_flat[idx_all]
 
-        # assign a weighted average to this bin
-        if idx_max.size + idx_min.size + idx_all.size > 0:
-            # normalize weighting to selected, active bins
-            weight[idx] = weight[idx] * bins_flat[idx]
-            weight = weight/nansum(weight)
+            # combine indices
+            idx = np.unique(np.concatenate((idx_min, idx_max, idx_all)))
 
-            # average
-            ave[i] = nansum(data_flat[idx]*weight[idx])
+            # assign a weighted average to this bin
+            if idx_max.size + idx_min.size + idx_all.size > 0:
+                # normalize weighting to selected, active bins
+                weight[idx] = weight[idx] * bins_flat[idx]
+                weight = weight/nansum(weight)
+
+                # average
+                ave[i] = nansum(data_flat[idx]*weight[idx])
+
+        else: # ;;wighout weighting by dphi and dtheta
+            id_array = np.argwhere(np.isfinite(data['data'])\
+                     & (data['bins'] == 1)\
+                     & (phi_array > phi_grid[i])\
+                     & (phi_array < phi_grid[i+1]))
+
+            if len(id_array) > 0:
+                ave[i] = np.nanmean(data['data'][id_array[:, 0].tolist(), id_array[:, 1].tolist()])
+
 
     # get y axis
     y = (phi_grid+shift(phi_grid, 1))/2.0
