eef577b788473d1e9732ed65e94cb3f6ae79909b
AARUESU <87346044+AARUESU@users.noreply.github.com>
2022-01-13 14:49:40 +0900

    Add processing for labels options, clip, in sdfit.py.
    In order to plot output adjustment.

--- ergpyspedas/erg/ground/radar/superdarn/sdfit.py
+++ ergpyspedas/erg/ground/radar/superdarn/sdfit.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from copy import deepcopy
 from pytplot import get_data, store_data, options, clip, ylim
+from pyspedas import tnames
 
 from ....satellite.erg.load import load
 
@@ -55,10 +56,10 @@ def sdfit(
                         +'/%Y/sd_fitacf_l2_'+site_input+'_%Y%m%d*.cdf'
 
         if notplot:
-            loaded_data.update(load(pathformat=pathformat, file_res=file_res, trange=trange, prefix=prefix, suffix='_'+site_input+suffix, get_support_data=get_support_data,
+            loaded_data.update(load(pathformat=pathformat, file_res=file_res, trange=trange, prefix=prefix, suffix=suffix, get_support_data=get_support_data,
                         varformat=varformat, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd))
         else:
-            loaded_data += load(pathformat=pathformat, file_res=file_res, trange=trange, prefix=prefix, suffix='_'+site_input+suffix, get_support_data=get_support_data,
+            loaded_data += load(pathformat=pathformat, file_res=file_res, trange=trange, prefix=prefix, suffix=suffix, get_support_data=get_support_data,
                         varformat=varformat, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
         """if (len(loaded_data) > 0) and ror:
             try:
@@ -88,16 +89,75 @@ def sdfit(
             except:
                 print('printing PI info and rules of the road was failed')
             
-        
+        """
+
         if (not downloadonly) and (not notplot):
 
-            tplot_name = prefix+'db_dt_' + site_input+suffix
-            if tplot_name in loaded_data:
-                clip(tplot_name, -1e+4, 1e+4)
-                get_data_vars = get_data(tplot_name)
-                ylim(tplot_name, np.nanmin(get_data_vars[1]), np.nanmax(get_data_vars[1]))
-                options(tplot_name, 'legend_names', ['dH/dt','dD/dt','dZ/dt'])
-                options(tplot_name, 'Color', ['b', 'g', 'r'])
-                options(tplot_name, 'ytitle', '\n'.join(tplot_name.split('_')))"""
+            t_plot_name_list = tnames([prefix + 'pwr*', prefix + 'spec*', prefix + 'vlos*'])
+            t_plot_name_list = list(set(t_plot_name_list).intersection(loaded_data))
+            for t_plot_name in t_plot_name_list:
+                clip(t_plot_name, -9000, 9000)
+            
+            t_plot_name_list = tnames([prefix + 'elev*'])
+            t_plot_name_list = list(set(t_plot_name_list).intersection(loaded_data))
+            if len(t_plot_name_list) > 5:
+                for t_plot_name in t_plot_name_list:
+                    clip(t_plot_name, -9000, 9000)
+
+            azim_no_name_list = list(set(tnames('*azim_no_?' + suffix)).intersection(loaded_data))
+            number_string_list = []
+            for azim_no_name in azim_no_name_list:
+                number_string_list.append(azim_no_name.split('_')[4][0])
+
+            for number_string in number_string_list:
+
+                site_input_upper = site_input.upper()
+                #  ;Set labels for some tplot variables
+                options(prefix + 'pwr_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'pwr_' + number_string + suffix, 'ztitle', 'Backscatter power [dB]')
+                options(prefix + 'pwr_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'pwr_err_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'pwr_err_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'pwr_err_' + number_string + suffix, 'ztitle', 'power err [dB]')
+                options(prefix + 'spec_width_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'spec_width_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'spec_width_' + number_string + suffix, 'ztitle', 'Spec. width [m/s]')
+                options(prefix + 'spec_width_err_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'spec_width_err_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'spec_width_err_' + number_string + suffix, 'ztitle', 'Spec. width err [m/s]')
+
+                if not prefix+'vlos_' + number_string + suffix in loaded_data:
+                    vlos_notplot_dictionary = load(pathformat=pathformat, file_res=file_res, trange=trange, prefix=prefix,suffix=suffix, get_support_data=get_support_data,
+                            varformat='vlos_' + number_string, downloadonly=downloadonly, notplot=True, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
+                    vlos_tplot_name = prefix+'vlos_' + number_string + suffix
+                    if len(vlos_notplot_dictionary) > 0:
+                        store_data(vlos_tplot_name,
+                                data={'x':vlos_notplot_dictionary[vlos_tplot_name]['x'],
+                                    'y':vlos_notplot_dictionary[vlos_tplot_name]['y'],
+                                    'v1':vlos_notplot_dictionary[vlos_tplot_name]['v'],
+                                    'v2':np.arange(vlos_notplot_dictionary[vlos_tplot_name]['y'].shape[2])},
+                                attr_dict={'CDF':vlos_notplot_dictionary[vlos_tplot_name]['CDF']})
+
+                        loaded_data.append(vlos_tplot_name)
+                options(prefix + 'vlos_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'vlos_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'vlos_' + number_string + suffix, 'ztitle', 'Doppler velocity [m/s]')
+
+                options(prefix + 'vlos_err_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'vlos_err_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'vlos_err_' + number_string + suffix, 'ztitle', 'Vlos err [m/s]')
+                options(prefix + 'elev_angle_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'elev_angle_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'elev_angle_' + number_string + suffix, 'ztitle', 'Elev. angle [deg]')
+                options(prefix + 'echo_flag_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'echo_flag_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'echo_flag_' + number_string + suffix, 'ztitle', '1: iono. echo')
+                options(prefix + 'quality_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'quality_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'quality_' + number_string + suffix, 'ztitle', 'quality')
+                options(prefix + 'quality_flag_' + number_string + suffix, 'ytitle', site_input_upper+'\nall beams')
+                options(prefix + 'quality_flag_' + number_string + suffix, 'ysubtitle', '[range gate]')
+                options(prefix + 'quality_flag_' + number_string + suffix, 'ztitle', 'quality flg')
+
 
     return loaded_data
