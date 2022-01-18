18ba0518522cdf6aef627678b2d70c5c999e2b64
AARUESU <87346044+AARUESU@users.noreply.github.com>
2022-01-18 10:23:41 +0900

    Fix processing for cdf_to_tplot(), in gmag_mm_210.py.
    In order to be able to get Tplot Variables of gmag_mm_210.

--- ergpyspedas/erg/ground/geomag/gmag_mm210.py
+++ ergpyspedas/erg/ground/geomag/gmag_mm210.py
@@ -23,8 +23,14 @@ def gmag_mm210(
     ror=True
 ):
 
-    site_code_all = ['msr', 'rik', 'kag', 'ktb', 'lcl', 'mdm', 'tew']
-    tres_all=['64hz', '1sec', '1min', '1h']
+    site_code_all = ['tik', 'zgn', 'yak', 'irt', 'ppi', 'bji',
+                    'lnp', 'mut', 'ptn', 'wtk', 'lmt', 'kat',
+                    'ktn', 'chd', 'zyk', 'mgd', 'ptk', 'msr',
+                    'rik', 'onw', 'kag', 'ymk', 'cbi', 'gua',
+                    'yap', 'kor', 'ktb', 'bik', 'wew', 'daw',
+                    'wep', 'bsv', 'dal', 'can', 'adl', 'kot',
+                    'cst', 'ewa', 'asa', 'mcq']
+    tres_all=['1sec', '1min', '1h']
     if isinstance(datatype, str):
         datatype = datatype.lower()
         datatype = datatype.split(' ')
@@ -38,10 +44,7 @@ def gmag_mm210(
     if len(datatype) < 1:
         return
 
-    if '64' in datatype:
-        index = np.where(np.array(datatype) == '64')[0][0]
-        datatype[index] = '64hz'
-    elif  '1s' in datatype:
+    if '1s' in datatype:
         index = np.where(np.array(datatype) == '1s')[0][0]
         datatype[index] = '1sec'
     elif  '1m' in datatype:
@@ -64,7 +67,7 @@ def gmag_mm210(
     
     site_code = list(set(site_code).intersection(site_code_all))
 
-    prefix = 'isee_fluxgate_'
+    prefix = 'mm210_'
     if notplot:
         loaded_data = {}
     else:
@@ -72,16 +75,13 @@ def gmag_mm210(
     for site_input in site_code:
         for data_type_in in datatype:
             fres = data_type_in
-            if fres == '64hz':
-                file_res = 3600.
-                pathformat = 'ground/geomag/isee/fluxgate/'+fres+'/'+site_input\
-                                +'/%Y/%m/isee_fluxgate_'+fres+'_'+site_input+'_%Y%m%d%H_v??.cdf'
+
             if fres == '1h':
                 fres = '1min'
-            if (fres == '1sec') or (fres == '1min'):
-                file_res = 3600. * 24
-                pathformat = 'ground/geomag/isee/fluxgate/'+fres+'/'+site_input\
-                                +'/%Y/isee_fluxgate_'+fres+'_'+site_input+'_%Y%m%d_v??.cdf'
+
+            file_res = 3600. * 24
+            pathformat = 'ground/geomag/mm210/'+fres+'/'+site_input\
+                            +'/%Y/mm210_'+fres+'_'+site_input+'_%Y%m%d_v??.cdf'
             
             loaded_data_temp = load(pathformat=pathformat, file_res=file_res, trange=trange, datatype=datatype, prefix=prefix, suffix='_'+site_input+suffix, get_support_data=get_support_data,
                             varformat=varformat, downloadonly=downloadonly, notplot=notplot, time_clip=time_clip, no_update=no_update, uname=uname, passwd=passwd)
@@ -90,7 +90,7 @@ def gmag_mm210(
                 loaded_data.update(loaded_data_temp)
             else:
                 loaded_data += loaded_data_temp
-            if (len(loaded_data_temp) > 0) and ror:
+            """if (len(loaded_data_temp) > 0) and ror:
                 try:
                     if isinstance(loaded_data_temp, list):
                         if downloadonly:
@@ -139,7 +139,7 @@ def gmag_mm210(
                             ylim(new_tplot_name, np.nanmin(get_data_vars[1]), np.nanmax(get_data_vars[1]))
                             options(new_tplot_name, 'legend_names', ['H','D','Z'])
                             options(new_tplot_name, 'Color', ['b', 'g', 'r'])
-                            options(new_tplot_name, 'ytitle', '\n'.join(new_tplot_name.split('_')))
+                            options(new_tplot_name, 'ytitle', '\n'.join(new_tplot_name.split('_')))"""
 
 
     return loaded_data
