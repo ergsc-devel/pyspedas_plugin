f6e336236121dcac32601e6557c9388430f8779a
AtsukiShinbori <86148190+shinbori@users.noreply.github.com>
2024-08-15 09:58:35 +0900

    Update load.py

--- ergpyspedas/erg/satellite/erg/load.py
+++ ergpyspedas/erg/satellite/erg/load.py
@@ -5,7 +5,7 @@ from pyspedas.utilities.dailynames import dailynames
 from pyspedas.utilities.download import download
 from pytplot import cdf_to_tplot
 
-from .config import CONFIG
+from ergpyspedas.erg.config import CONFIG
 
 def load(trange=['2017-03-27', '2017-03-28'],
          pathformat=None,
@@ -27,7 +27,8 @@ def load(trange=['2017-03-27', '2017-03-28'],
          uname=None,
          passwd=None,
          time_clip=False,
-         version=None):
+         version=None,
+         force_download=False):
     """
     This function is not meant to be called directly; please see the instrument specific wrappers:
         pyspedas.erg.mgf()
@@ -50,7 +51,7 @@ def load(trange=['2017-03-27', '2017-03-28'],
     out_files = []
 
     files = download(remote_file=remote_names, remote_path=CONFIG['remote_data_dir'], local_path=CONFIG[
-                     'local_data_dir'], no_download=no_update, last_version=True, username=uname, password=passwd)
+                     'local_data_dir'], no_download=no_update, last_version=True, username=uname, password=passwd, force_download=force_download)
     if files is not None:
         for file in files:
             out_files.append(file)
@@ -60,6 +61,12 @@ def load(trange=['2017-03-27', '2017-03-28'],
     if downloadonly:
         return out_files
 
+    new_cdflib = False
+    if cdflib.__version__ > "0.4.9":
+        new_cdflib = True
+    else:
+        new_cdflib = False
+
     tvars = cdf_to_tplot(out_files, prefix=prefix, suffix=suffix, get_support_data=get_support_data,
                          varformat=varformat, varnames=varnames, notplot=notplot)
 
@@ -67,7 +74,10 @@ def load(trange=['2017-03-27', '2017-03-28'],
         if len(out_files) > 0:
             cdf_file = cdflib.CDF(out_files[-1])
             cdf_info = cdf_file.cdf_info()
-            all_cdf_variables = cdf_info['rVariables'] + cdf_info['zVariables']
+            if new_cdflib:
+                all_cdf_variables = cdf_info.rVariables + cdf_info.zVariables
+            else:
+                all_cdf_variables = cdf_info["rVariables"] + cdf_info["zVariables"]
             gatt = cdf_file.globalattsget()
             for var in all_cdf_variables:
                 t_plot_name = prefix + var + suffix
