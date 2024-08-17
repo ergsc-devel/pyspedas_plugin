de233a1f78a0dd9f082cfaa2c44f3719b4a096b5
Atsuki Shinbori <shinbori@isee.nagoya-u.ac.jp>
2024-08-17 19:16:17 +0900

    Update some programs

--- ergpyspedas/erg/ground/camera/omti/camera_omti_asi.py
+++ ergpyspedas/erg/ground/camera/omti/camera_omti_asi.py
@@ -2,8 +2,8 @@ import cdflib
 import numpy as np
 
 from pytplot import get_data, store_data, options, clip, ylim
-from ...satellite.erg.load import load
-from ...satellite.erg.get_gatt_ror import get_gatt_ror
+from ....satellite.erg.load import load
+from ....satellite.erg.get_gatt_ror import get_gatt_ror
 
 from typing import List, Union, Optional
 
