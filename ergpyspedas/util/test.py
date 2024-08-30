a35c4e15ec8fde161ec3f76026b5facd85bad177
Tomo Hori @ISEE <tomo.hori at nagoya-u.jp>
2024-08-30 14:43:31 +0900

    initial import: tplot_vl.py and related routines

---
+++ ergpyspedas/util/test.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from ergpyspedas.erg import mgf, orb
+mgf()
+orb()
+from pytplot import tplot_options, options, tplot_names, split_vec, get_data, tplot_opt_glob, tnames
+
+split_vec('erg_orb_l2_pos_rmlatmlt')
+split_vec('erg_orb_l2_pos_Lm')
+options('erg_orb_l2_pos_rmlatmlt_x', 'ytitle', 'R')
+options('erg_orb_l2_pos_rmlatmlt_y', 'ytitle', 'Mlat')
+options('erg_orb_l2_pos_rmlatmlt_z', 'ytitle', 'MLT')
+
+var_label=['erg_orb_l2_pos_Lm_x', 'erg_orb_l2_pos_rmlatmlt_x','erg_orb_l2_pos_rmlatmlt_y','erg_orb_l2_pos_rmlatmlt_z']
+tplot_options('var_label', var_label)
+
+plot_vars=['erg_mgf_l2_mag_8sec_sm','erg_mgf_l2_igrf_8sec_sm','erg_orb_l2_pos_Lm_x']
+
+from ergpyspedas.util import tplot_vl
+fig = tplot_vl(plot_vars)
+
