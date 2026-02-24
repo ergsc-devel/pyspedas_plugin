98697ad0e8f582e5ae2ebd9666abc9bfc3f0aef0
AtsukiShinbori <86148190+shinbori@users.noreply.github.com>
2026-02-24 19:32:34 +0900

    Update tzwo_absint_nobg.py

--- ergpyspedas/erg/ground/camera/zwo/tzwo_absint_nobg.py
+++ ergpyspedas/erg/ground/camera/zwo/tzwo_absint_nobg.py
@@ -137,7 +137,8 @@ def tzwo_absint_nobg(
         print('now converting...:', time_string(times_ag[i]))
 
     # storing data to several plots
-    store_data(f'{v_name1[:24]}abs', data={'x': times_ag, 'y': abs_img_ag_int}, attr_dict=var_attrs)
+    store_data(f'{v_name1[:23]}abs', data={'x': times_ag, 'y': abs_img_ag_int}, attr_dict=var_attrs)
+
 
 
 
